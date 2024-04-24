1bfab24cd05b2ff2227ce46c098198d394e217cc
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2024-04-23 21:42:27 -0500

    Added data and operational functions

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
-    #df = pandas.read_csv("")
-    #temperature = df.station(date)
-    temerature = 23
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
     return {
         "station": station,
         "date": date,
-        "temperature": temerature
+        "temperature": temperature
     }
+
 
 if __name__ == "__main__":
     app.run(debug=True)
